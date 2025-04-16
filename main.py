73c838bf6c16d2fdcd150031f1dd71d23b2d711e
Evgeny B <evgeny.bubnov.13@gmail.com>
2025-04-16 19:29:46 +0300

    structure

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+
+
+def main() -> None:
+    """
+    Main function to run the script.
+    """
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
