724f8f62f8b5119729f294f5824106cdc99c36c8
Evgeny B <evgeny.bubnov.13@gmail.com>
2025-04-20 20:18:39 +0300

    cancel

---
+++ bot_logic/handler_cancel.py
@@ -0,0 +1,27 @@
+from telegram import Update
+from telegram.ext import ContextTypes, ConversationHandler
+
+async def cancel_callback(query, context: ContextTypes.DEFAULT_TYPE):
+    await query.answer()
+    context.user_data.setdefault("messages_to_delete", []).append(query.message.message_id)
+
+    chat_id = query.message.chat_id
+    for msg_id in context.user_data["messages_to_delete"]:
+        try:
+            await context.bot.delete_message(chat_id=chat_id, message_id=msg_id)
+        except Exception as e:
+            print(f"Failed to delete message {msg_id}: {e}")
+
+    return ConversationHandler.END
+
+async def cancel_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data.setdefault("messages_to_delete", []).append(update.message.message_id)
+    chat_id = update.effective_chat.id
+
+    for msg_id in context.user_data["messages_to_delete"]:
+        try:
+            await context.bot.delete_message(chat_id=chat_id, message_id=msg_id)
+        except Exception as e:
+            print(f"Failed to delete message {msg_id}: {e}")
+
+    return ConversationHandler.END
--- bot_logic/tg_bot.py
+++ bot_logic/tg_bot.py
@@ -2,6 +2,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 from bot_logic.handler_help import help_command
 from bot_logic.handler_start import start_command
 from bot_logic.handler_location import ask_type, save_location, WAITNG_FOR_TYPE
+from bot_logic.handler_cancel import cancel_callback, cancel_command
 import settings
 
 BOT_TOKEN = settings.BOT_TOKEN
@@ -11,6 +12,7 @@ def bot_start() -> None:
 
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("cancel", cancel_command))
 
     conv_handler = ConversationHandler(
         
